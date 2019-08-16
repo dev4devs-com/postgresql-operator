33dcd3281a00fcd3a3663b01a3672714c798d1e2
Camila Macedo <camilamacedo86@gmail.com>
2019-08-16 17:33:22 +0100

    feat(test) : add e2e test

--- go.mod
+++ go.mod
@@ -2,10 +2,12 @@ module github.com/dev4devs-com/postgresql-operator
 
 require (
 	github.com/NYTimes/gziphandler v1.0.1 // indirect
+	github.com/aerogear/unifiedpush-operator v0.0.0-20190813172417-3b1c6f82ea7d
 	github.com/go-logr/logr v0.1.0
 	github.com/go-openapi/spec v0.19.0
 	github.com/operator-framework/operator-sdk v0.10.1-0.20190809191117-c1e2eae6580e
 	github.com/spf13/pflag v1.0.3
+	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3
 	k8s.io/api v0.0.0-20190612125737-db0771252981
 	k8s.io/apimachinery v0.0.0-20190612125636-6a5db36e93ad
 	k8s.io/client-go v11.0.0+incompatible
---
+++ test/e2e/postgresql_test.go
@@ -0,0 +1,66 @@
+package e2e
+
+import (
+	"github.com/dev4devs-com/postgresql-operator/pkg/apis/postgresqloperator/v1alpha1"
+	framework "github.com/operator-framework/operator-sdk/pkg/test"
+	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+	"github.com/operator-framework/operator-sdk/test/test-framework/pkg/apis"
+	"golang.org/x/net/context"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"testing"
+	"time"
+)
+
+var (
+	retryInterval        = time.Second * 5
+	timeout              = time.Second * 300
+	cleanupRetryInterval = time.Second * 1
+	cleanupTimeout       = time.Second * 5
+)
+
+
+
+func TestPostgreSQL(t *testing.T) {
+	ctx := framework.NewTestCtx(t)
+	defer ctx.Cleanup()
+	f := framework.Global
+
+	postgresqlList := &v1alpha1.PostgresqlList{}
+	if err := framework.AddToFrameworkScheme(apis.AddToScheme, postgresqlList); err != nil {
+		t.Fatalf("failed to add custom resource scheme to framework: %v", err)
+	}
+
+	backupList := &v1alpha1.BackupList{}
+	if err := framework.AddToFrameworkScheme(apis.AddToScheme, backupList); err != nil {
+		t.Fatalf("failed to add custom resource scheme to framework: %v", err)
+	}
+
+	namespace, err := ctx.GetNamespace()
+	if err != nil {
+		t.Fatalf("failed to get namespace: %v", err)
+	}
+
+	postgresql := &v1alpha1.Postgresql{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:     "test-postgresql",
+			Namespace: namespace,
+		},
+	}
+
+	err = f.Client.Create(context.TODO(), postgresql, &framework.CleanupOptions{
+		TestContext:   ctx,
+		Timeout:       timeout,
+		RetryInterval: retryInterval,
+	})
+
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+
+	// wait for postgresql-operator to be ready
+	err = e2eutil.WaitForOperatorDeployment(t, f.KubeClient, namespace, "postgresql-operator", 1, time.Second*5, time.Second*30)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}
